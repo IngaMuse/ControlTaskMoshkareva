553f5ba2a97a3066cf107fa0912d338909bdcbf7
IngaMuse <ingamoshkareva@gmail.com>
2024-11-27 18:56:16 +0300

    added button name student

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -4,8 +4,9 @@ sap.ui.define([
 		"sap/ui/model/json/JSONModel",
 		"zjblessons/ControlTaskMoshkareva/model/formatter",
 		"sap/ui/model/Filter",
-		"sap/ui/model/FilterOperator"
-	], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+		"sap/m/MessageBox"
+	], function (BaseController, JSONModel, formatter, Filter, FilterOperator, MessageBox) {
 		"use strict";
 
 		return BaseController.extend("zjblessons.ControlTaskMoshkareva.controller.Worklist", {
@@ -19,10 +20,23 @@ sap.ui.define([
         this.setModel(oViewModel, "worklistView");
 			},
 			
-				
 			onSearchFieldLiveChange: function (oEvent) {
+			},
+			
+			onShowStudentInfoPress: function () {
+						MessageBox.show(
+								"ФИО студента: Инга Мошкарёва",
+								{
+										icon: MessageBox.Icon.INFORMATION,
+										title: "Студент",
+										actions: [MessageBox.Action.OK],
+										onClose: function (oAction) {
+										}
+								}
+						);
 				
-		}
+		  }
+
 
 		});
 	}
